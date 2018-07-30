93ee685e6071cd557a86b3740f9b0936b8f3baf5
Carl Waldbieser <waldbiec@lafayette.edu>
2018-07-30 15:46:17 -0400

    Removed obsolete files.

--- full_sync/full_sync_jobs.sh
+++ full_sync/full_sync_jobs.sh
@@ -1,61 +1,20 @@
 #! /bin/bash
 
+THISDIR="$( cd $(dirname $0); pwd)"
 PYTHON=/opt/python27/bin/python
-FILTER_SSH_GROUPS=/home/grouper/full_sync/filter_ssh_groupmap.py
-SYNCHRONIZE=/home/grouper/full_sync/synchronize.sh
+SYNC_GROUPS="$THISDIR/sync_groups.sh"
+EXCLUDE_GROUPS="$THISDIR/exclude_groups"
+LIST_EXPORTS="$THISDIR/list_exports"
 LOG=/var/log/grouper_tools/full_sync.log
+
 function tstamp
 {
     date '+%Y-%m-%dT%H:%M:%S'
 }
 
-ALL_EXPORTS=$(mktemp)
-/home/grouper/full_sync/list_export_groups > "$ALL_EXPORTS"
-
-# Mailman
-echo "["$(tstamp)"] Starting mailman group sync ..." >> "$LOG"
-"$PYTHON" "$FILTER_SSH_GROUPS" /etc/txamqpprovisioners/provisioners/maillists/groupmap.json "$ALL_EXPORTS" | \
-    "$SYNCHRONIZE" > /dev/null 2>> "$LOG"
-echo >> "$LOG"
-
-# Share-o-matic
-echo "["$(tstamp)"] Starting share-o-matic group sync ..." >> "$LOG"
-"$PYTHON" "$FILTER_SSH_GROUPS" /etc/txamqpprovisioners/provisioners/shareomatic/volume_its/groupmap.json "$ALL_EXPORTS" | \
-    "$SYNCHRONIZE" > /dev/null 2>> "$LOG"
-echo >> "$LOG"
-
-# Zimbra distribution lists.
-echo "["$(tstamp)"] Starting Zimbra distribution list group sync ..." >> "$LOG"
-"$PYTHON" "$FILTER_SSH_GROUPS" /etc/txamqpprovisioners/provisioners/zimbra/groupmap.json "$ALL_EXPORTS" | \
-    "$SYNCHRONIZE" > /dev/null 2>> "$LOG"
-echo >> "$LOG"
-
-# LDAP
-echo "["$(tstamp)"] Starting LDAP group sync ..." >> "$LOG"
-/home/grouper/full_sync/match_export_groups | \
-    /home/grouper/full_sync/exclude_large_groups | \
-        "$SYNCHRONIZE" > /dev/null 2>> "$LOG"
-echo >> "$LOG"
-
-# Board Effect (Workrooms)
-# BE general access is already triggered during LDAP full sync.
-echo "["$(tstamp)"] Starting Board Effect group sync ..." >> "$LOG"
-/home/grouper/full_sync/list_be_workroom_groups.py | \
-    "$SYNCHRONIZE" > /dev/null 2>> "$LOG"
-echo >> "$LOG"
-
-# Slack (UserGroups)
-echo "["$(tstamp)"] Starting Slack group sync ..." >> "$LOG"
-/home/grouper/full_sync/list_slack_usergroups.py | \
-    "$SYNCHRONIZE" > /dev/null 2>> "$LOG"
-echo >> "$LOG"
-
-# o365
-echo "["$(tstamp)"] Starting o365 group sync ..." >> "$LOG"
-/home/grouper/full_sync/list_o365_groups.py | \
-    "$SYNCHRONIZE" > /dev/null 2>> "$LOG"
-echo >> "$LOG"
+echo "[$(tstamp)][INFO] Starting exports sync ..." > "$LOG"
+"$LIST_EXPORTS" | "$EXCLUDE_GROUPS" | "$SYNC_GROUPS" -
+echo "[$(tstamp)][INFO] Starting exports sync ..." >> "$LOG"
 
 
-rm "$ALL_EXPORTS"
 
--- full_sync/sync_groups.sh
+++ full_sync/sync_groups.sh
@@ -10,12 +10,26 @@ MAKE_MESSAGES="$THISDIR/get_members_for_groups"
 TXQPRODUCER="/var/opt/txamqp_tools/txqproducer"
 MAX_SUBJECTS_PER_GROUP=${MAX_SUBJECTS_PER_GROUP:-200}
 
+function tstamp
+{
+    date '+%Y-%m-%dT%H:%M:%S'
+}
+
 if [ -z "$1" ]; then
     echo "Usage: $0 GROUP_NAMES_FILE" >&2
     exit 1
 fi
 
-"$MAKE_MESSAGES" "$1" --max-subjects "$MAX_SUBJECTS_PER_GROUP" | while read jsonstr; do
-    echo "$jsonstr" | \
-        "$TXQPRODUCER" "$GROUPER_EXCHANGE" "$ROUTE_KEY" - -e "$EXCHANGE_ENDPOINT" -u "$EXCHANGE_USER" --passwd-file "$EXCHANGE_PASSWD_FILE"
-done
+TEMP_JSON=$(mktemp)
+if "$MAKE_MESSAGES" "$1" --max-subjects "$MAX_SUBJECTS_PER_GROUP" > "$TEMP_JSON"; then
+
+    while read jsonstr; do
+        echo "$jsonstr" | \
+            "$TXQPRODUCER" "$GROUPER_EXCHANGE" "$ROUTE_KEY" - -e "$EXCHANGE_ENDPOINT" -u "$EXCHANGE_USER" --passwd-file "$EXCHANGE_PASSWD_FILE"
+    done < "$TEMP_JSON"
+    rm "$TEMP_JSON"
+else
+    echo "[$(tstamp)][ERROR] Error looking up group memberships."
+    rm "$TEMP_JSON"
+fi
+
